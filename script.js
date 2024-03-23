5287d5e04329aad4709ae4dc82812314ada502c8
hoaganh <161003856+hoaganh@users.noreply.github.com>
2024-03-23 11:33:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-
+ <div id="wrapper">
+        <div id="header">
+            <a href="" class="logo">
+                <img src="./logo.png" alt="">
+            </a>
+            <div id="menu">
+                <div class="item">
+                    <a href="">Trang chủ</a>
+                </div>
+                <div class="item">
+                    <a href="">Sản phẩm</a>
+                </div>
+                <div class="item">
+                    <a href="">Blog</a>
+                </div>
+                <div class="item">
+                    <a href="">Liên hệ</a>
+                </div>
+            </div>
+            <div id="actions">
+                <div class="item">
+                    <img src="./user.png" alt="">
+                </div>
+                <div class="item">
+                    <img src="./cart.png" alt="">
+                </div>
+            </div>
+        </div>
